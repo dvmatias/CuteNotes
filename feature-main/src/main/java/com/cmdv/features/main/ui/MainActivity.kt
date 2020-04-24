f2a173295613a6c29ea887905f5bd94d0889d854
Matías <matias.delv.dom@gmail.com>
2020-04-23 21:25:33 -0300

    Added toggle icon.

--- feature-main/src/main/java/com/cmdv/features/main/ui/MainActivity.kt
+++ feature-main/src/main/java/com/cmdv/features/main/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.DisplayMetrics
 import android.view.*
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
@@ -41,7 +42,7 @@ class MainActivity : AppCompatActivity(), NavRecyclerAdapter.OnNavigationItemCli
 
 		setSupportActionBar(toolbar)
 		setupDrawerLayout()
-		setupNavigationDrawerRecycler()
+		setupNavRecycler()
 		setupStatusBar()
 	}
 
@@ -67,6 +68,8 @@ class MainActivity : AppCompatActivity(), NavRecyclerAdapter.OnNavigationItemCli
 		params.width = (displayMetrics.widthPixels * 0.85).toInt()
 		navView.layoutParams = params
 
+		val drawerToggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+
 //		val drawerToggle = object : ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
 //			override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
 //				super.onDrawerSlide(drawerView, slideOffset)
@@ -74,16 +77,18 @@ class MainActivity : AppCompatActivity(), NavRecyclerAdapter.OnNavigationItemCli
 //				appBarMain.translationX = moveFactor
 //			}
 //		}
-//
-//		drawerLayout.addDrawerListener(drawerToggle)
-//		drawerToggle.syncState()
-//		navView.setNavigationItemSelectedListener(null)
+
+		drawerLayout.addDrawerListener(drawerToggle)
+		drawerToggle.syncState()
+		navView.setNavigationItemSelectedListener(null)
 	}
 
-	private fun setupNavigationDrawerRecycler() {
+	private fun setupNavRecycler() {
 		navRecycler.layoutManager = LinearLayoutManager(this)
 		this.navRecyclerAdapter = NavRecyclerAdapter(this, this)
 		navRecycler.adapter = this.navRecyclerAdapter
+
+		navRecycler.addItemDecoration(NavRecyclerItemDecoration(this))
 	}
 
 	override fun onClick(item: NavRecyclerAdapter.NavItemModel, position: Int) {
---
+++ feature-main/src/main/java/com/cmdv/features/main/ui/NavRecyclerItemDecoration.kt
@@ -0,0 +1,65 @@
+package com.cmdv.features.main.ui
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.cmdv.features.R
+
+class NavRecyclerItemDecoration(private val context: Context): RecyclerView.ItemDecoration() {
+	private var divider: Drawable = ContextCompat.getDrawable(context, R.drawable.divider_nav_recycler)!!
+
+	override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+		val left = parent.paddingLeft + 60
+		val right = parent.width - parent.paddingRight - 40
+
+		for (i in 0 until parent.childCount) {
+			if (i == 3) {
+				val child: View = parent.getChildAt(i)
+
+				val params: RecyclerView.LayoutParams = child.layoutParams as RecyclerView.LayoutParams
+
+				val top = child.bottom + params.bottomMargin
+				val bottom = top + divider.intrinsicHeight
+
+				divider.setBounds(left, top, right, bottom)
+				divider.draw(c)
+			}
+		}
+	}
+}
+
+/*
+
+
+public class SimpleDividerItemDecoration extends RecyclerView.ItemDecoration {
+    private Drawable mDivider;
+
+    public SimpleDividerItemDecoration(Context context) {
+        mDivider = context.getResources().getDrawable(R.drawable.line_divider);
+    }
+
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        int left = parent.getPaddingLeft();
+        int right = parent.getWidth() - parent.getPaddingRight();
+
+        int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = parent.getChildAt(i);
+
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+            int top = child.getBottom() + params.bottomMargin;
+            int bottom = top + mDivider.getIntrinsicHeight();
+
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
+    }
+}
+
+
+ */
